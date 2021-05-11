81195992c1de537e153cdab4735a9300b240f798
Luan (Mike) <1703843+mikedark@users.noreply.github.com>
2021-05-11 18:25:04 -0300

    Base já feita

--- POO-2020-2/src/CodNumBelowException.java
+++
@@ -1,8 +0,0 @@
-//Nome: Luan Emanuel da Silva - RA: 2143860
-
-public class CodNumBelowException extends Exception{
-
-    public void printBelowException(){
-        System.out.println("\nO numero do código não pode ser negativo!\n");
-    }
-}
