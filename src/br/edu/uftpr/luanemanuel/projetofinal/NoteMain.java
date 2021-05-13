fb7c6f6f51cb2d2325ad0ecf3adc14e98c17704e
Luan (Mike) <1703843+mikedark@users.noreply.github.com>
2021-05-12 21:03:03 -0300

    Revert "*Iniciando a criação da interface gráfica: -Criado interface de menu, menu de cadastramento e o cadastramento da empresa -Programado o cadastramento da empresa"
    This reverts commit 14127607

---
+++ src/br/edu/uftpr/luanemanuel/projetofinal/NoteMain.java
@@ -0,0 +1,10 @@
+//Nome: Luan Emanuel da Silva - RA: 2143860
+package br.edu.uftpr.luanemanuel.projetofinal;
+
+public class NoteMain {
+
+    public static void main(String[] args) {
+        NoteMenu noteMenu = new NoteMenu();
+        noteMenu.generateMenu();
+    }
+}
